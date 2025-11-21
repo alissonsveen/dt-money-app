75ee0b1361c2e0de635dac0806d5165cec867c0d
alissonsveen <alissongamer6@gmail.com>
2025-11-21 20:02:22 -0300

    chore: Cadastro de novas transações - Exibindo mensagens de erro

--- src/components/NewTransaction/index.tsx
+++ src/components/NewTransaction/index.tsx
@@ -10,6 +10,7 @@ import { SelectCategoryModal } from "../SelectCategoryModal"
 import { transactionSchema } from "./schema"
 import * as Yup from "yup"
 import { AppButton } from "../AppButton"
+import { ErrorMessage } from "../ErrorMessage"
 
 type ValidationErrosTypes = Record<keyof CreateTransactionInterface, string>
 
@@ -68,6 +69,9 @@ export const NewTransaction = () => {
           value={transaction.description}
           className="text-white text-lg h-[50px] bg-background-primary my-2 rounded-md pl-4"
         />
+        {validationErrors?.description && (
+          <ErrorMessage>{validationErrors.description}</ErrorMessage>
+        )}
         <CurrencyInput
           value={transaction.value}
           prefix="R$ "
@@ -78,17 +82,26 @@ export const NewTransaction = () => {
           className="text-white text-lg h-[50px] bg-background-primary my-2 rounded-md pl-4"
           onChangeValue={(value) => setTransactionData("value", value ?? 0)}
         />
+        {validationErrors?.value && (
+          <ErrorMessage>{validationErrors.value}</ErrorMessage>
+        )}
         <SelectCategoryModal
           selectedCategory={transaction.categoryId}
           onSelect={(categoryId) =>
             setTransactionData("categoryId", categoryId)
           }
         />
+        {validationErrors?.categoryId && (
+          <ErrorMessage>{validationErrors.categoryId}</ErrorMessage>
+        )}
 
         <TransactionTypeSelector
           typeId={transaction.typeId}
           setTransactionType={(typeId) => setTransactionData("typeId", typeId)}
         />
+        {validationErrors?.typeId && (
+          <ErrorMessage>{validationErrors.typeId}</ErrorMessage>
+        )}
 
         <View className="my-4">
           <AppButton onPress={handleCreateTransaction}>
